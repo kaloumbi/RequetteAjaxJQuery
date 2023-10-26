c8cc01498060eb029fd91ec6d60179f7ae9ab38f
Paterne <diedhioupaterne279@gmail.com>
2023-10-26 13:28:31 +0000

    Requette avec la methode Ajax en JQuery

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -25,14 +25,63 @@ $(function () {
      */
 
 
-/**
-        $.post(url, function (response, status, xhr) {
-        console.log({ response, status, xhr });
-        if (status == "success" && response.isSuccess) {
-            const users = response.results
+    /**
+            $.post(url, function (response, status, xhr) {
+            console.log({ response, status, xhr });
+            if (status == "success" && response.isSuccess) {
+                const users = response.results
+    
+                users.forEach((user, index) => {
+                    $('.users').append(`
+                        <div class="user-item">
+                        <div class="user-name"> Name: ${user.name} </div>
+                        <div class="user-email"> Email: ${user.email} </div>
+                        <div class="user-address"> Adresse: ${user.address} </div>
+                        <div class="user-city"> City: ${user.city} </div>
+                        <div class="user-country"> Country: ${user.country} </div>
+                    </div>
+                    `)
+                });
+                console.log(users);
+            }
+            //console.log({data, status, xhr});
+        },'json'
+        )
+        */
 
-            users.forEach((user, index) => {
-                $('.users').append(`
+
+    /** +++++++++++++++++  AJAX JQUERY  +++++++++++++++++++++++++++                         */
+
+
+    // $.ajax(url, {options}); ou $.ajax({options});
+    // Paramétrer sa requête
+    // Voici les options les plus utilisées :
+
+    // Option	     =>    Description
+    // URL	         =>    Adresse à laquelle la requête est envoyée
+    // type	         =>    Le type de requête GET (par défaut) ou POST
+    // data	         =>    Les données à envoyer au serveur
+    // datatype	     =>    Le type de données pouvant être transmises au serveur : 
+    //               =>    php, html, script, json et xml
+    // success	     =>    La fonction à appeler si la requête a abouti
+    // error	     =>    La fonction à appeler si la requête n'a pas abouti
+    // timeout	     =>    Le délai maximum en millisecondes de traitement de la demande. 
+    //               =>    Passé ce délai, elle retourne le paramètre error.
+
+
+
+    const urlApi = "/api/users.json"
+    $.ajax(
+        {
+            url: urlApi,
+            type: "GET",
+            success: function (response, status, xhr) {
+
+                if (status == "success" && response.isSuccess) {
+                    const users = response.results
+
+                    users.forEach((user, index) => {
+                        $('.users').append(`
                     <div class="user-item">
                     <div class="user-name"> Name: ${user.name} </div>
                     <div class="user-email"> Email: ${user.email} </div>
@@ -41,39 +90,17 @@ $(function () {
                     <div class="user-country"> Country: ${user.country} </div>
                 </div>
                 `)
-            });
-            console.log(users);
+                    });
+                    //console.log(users);
+                }
+                //console.log({data, status, xhr});
+            },
+            error: function (error) {
+                console.log(error.statusText);
+            }
         }
-        //console.log({data, status, xhr});
-    },'json'
-    )
-    */
-
-    
-
-    const url = "/api/users.json"
-    $.get(url, function (response, status, xhr) {
-        console.log({ response, status, xhr });
-        if (status == "success" && response.isSuccess) {
-            const users = response.results
-
-            users.forEach((user, index) => {
-                $('.users').append(`
-                    <div class="user-item">
-                    <div class="user-name"> Name: ${user.name} </div>
-                    <div class="user-email"> Email: ${user.email} </div>
-                    <div class="user-address"> Adresse: ${user.address} </div>
-                    <div class="user-city"> City: ${user.city} </div>
-                    <div class="user-country"> Country: ${user.country} </div>
-                </div>
-                `)
-            });
-            console.log(users);
-        }
-        //console.log({data, status, xhr});
-    },'json'
     )
 
-    
+
 
 })
